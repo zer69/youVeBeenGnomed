cc6aeeb4738de0bb8d4d83b21754f1bc6927f270
MaxxZahar <maxxzahar@rambler.ru>
2023-01-16 20:35:17 +0300

    Player controller unrefined update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,19 +13,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundDrag; 
     [SerializeField] private float angleVelocity;
     [SerializeField] private float maxVelocity;
+    [SerializeField] private float mouseSensetivity;
+    private GameObject playerCamera;
     private Rigidbody playerRb;
     private bool onGround;
+    private float xRotation = 0;
     private LayerMask isGround;
+    private Vector2 lookCommand = Vector2.zero;
     private Vector2 moveCommand = Vector2.zero;
-    private Vector3 angleVelocityV;
     private bool jumping = false;
     // Start is called before the first frame update
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerCamera = GameObject.Find("Main Camera");
         playerRb.mass = 70;
         playerInput.onActionTriggered += OnPlayerInputActionTriggered;
-        angleVelocityV = new Vector3(0, angleVelocity, 0);
+        isGround = LayerMask.GetMask("isGround");
     }
 
     private void OnPlayerInputActionTriggered(InputAction.CallbackContext context)
@@ -39,38 +43,62 @@ public class PlayerController : MonoBehaviour
             case "Jump":
                 jumping = true;
                 break;
+
+            case "Look":
+                lookCommand = context.action.ReadValue<Vector2>();
+                break;
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (lookCommand != Vector2.zero)
+        {
+            Debug.Log(transform.position);
+            float xMouse = lookCommand.x * mouseSensetivity * Time.deltaTime;
+            float yMouse = lookCommand.y * mouseSensetivity * Time.deltaTime;
+
+            xRotation -= yMouse;
+            xRotation = Mathf.Clamp(xRotation, -90, 90);
+            playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            transform.Rotate(Vector3.up * xMouse);
+        } 
+    }
+
+    private void FixedUpdate()
+    {
+        onGround = Physics.Raycast(transform.position, Vector3.down, playerHeight + 0.2f, isGround);
         if (moveCommand != Vector2.zero)
         {
-            onGround = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, isGround);
-            // transform.position += (transform.forward * moveCommand.y + transform.right * moveCommand.x) * speed * Time.deltaTime;
-            Vector3 direction = new Vector3(moveCommand.x, 0, moveCommand.y);
-            // playerRb.AddForce(direction, ForceMode.Impulse);
-            // Vector3 direction = orientation.forward * moveCommand.y + orientation.right * moveCommand.x;
-            // playerRb.velocity = direction;
-            Quaternion deltaRotation = Quaternion.Euler(angleVelocityV * moveCommand.x * Time.deltaTime);
-            SpeedControl();
-            if (onGround)
-            {
-                playerRb.drag = groundDrag;
-            } else
-            {
-                playerRb.drag = 0;
-            }
-            // transform.Rotate(0, moveCommand.x * rotationSpeed * Time.deltaTime, 0);
-            playerRb.MoveRotation(playerRb.rotation * deltaRotation);
-            playerRb.AddForce(direction * speed, ForceMode.Force);
+            Move();
         }
-        if (jumping)
+        if (jumping && onGround)
         {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumping = false;
+            Jump();
         }
+    }
+
+    private void Move()
+    {
+        Vector3 direction = new Vector3(moveCommand.x, 0, moveCommand.y);
+    
+        SpeedControl();
+
+        if (onGround)
+        {
+            playerRb.drag = groundDrag;
+        }
+        else
+        {
+            playerRb.drag = 0;
+        }
+        playerRb.AddForce(direction * speed, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        jumping = false;
     }
 
     private void SpeedControl()
@@ -82,6 +110,5 @@ public class PlayerController : MonoBehaviour
             Vector3 limitedVelocity = moveVelocity.normalized * maxVelocity;
             playerRb.velocity = new Vector3(limitedVelocity.x, playerRb.velocity.y, limitedVelocity.z);
         }
-        Debug.Log(playerRb.velocity.magnitude);
     }
 }
