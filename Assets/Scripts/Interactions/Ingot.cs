d3b4b122d0eb2b6419fedbafc9055313bacfe109
zer69 <zeeroybox@gmail.com>
2023-01-23 13:27:55 +0300

    Ingot prefab&script added

---
+++ Assets/Scripts/Interactions/Ingot.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingot : MonoBehaviour
+{
+    enum Rarity
+    {
+        Common,
+        Uncommon,
+        Rare, 
+        Epic, 
+        Legendary
+    };
+
+    enum OreType
+    {
+        Iron,
+        Steel,
+        Copper,
+        Bronze
+    };
+
+    enum Enchantment
+    {
+        None,
+        Fire,
+        Water,
+        Earth,
+        Air,
+        Poison,
+        Lightning,
+        Light,
+        Dark
+    };
+
+    enum CompletionStatus
+    {
+        Raw,
+        Melted,
+        Forged,
+        Cooled,
+        Sharpened,
+        Completed
+
+    };
+
+    [SerializeField] private CompletionStatus status;
+
+    [SerializeField] private Rarity rarity;
+    [SerializeField] private OreType type;
+    [SerializeField] private float quality;
+
+    [SerializeField] private float MeltingPoint;
+    [SerializeField] private float currentTemperature;
+
+    [SerializeField] private float strength;
+    [SerializeField] private float fragility;
+
+    [SerializeField] private float sharpness;
+
+    [SerializeField] bool isEnchanted;
+    [SerializeField] private Enchantment enchantment;
+    [SerializeField] private float enchantmentQuality;
+
+    private float price; // calculated based on rarity, type, quality, strength, fragility, sharpness and enchantment
+
+
+}
