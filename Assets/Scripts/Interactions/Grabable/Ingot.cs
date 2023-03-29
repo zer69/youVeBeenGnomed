7bc1b1e65fa03a5b45e4acda192a8ab1ec2f8242
call_me_zer <76487961+zer69@users.noreply.github.com>
2023-03-29 20:37:15 +0300

    ingot status update

--- Assets/Scripts/Interactions/Grabable/Ingot.cs
+++ Assets/Scripts/Interactions/Grabable/Ingot.cs
@@ -37,12 +37,16 @@ public class Ingot : MonoBehaviour
 
     public enum CompletionStatus
     {
-        Raw,
-        Melted,
-        Forged,
-        Cooled,
-        Sharpened,
-        Completed
+        Raw, // status for furnace
+        Melted, // status after furnace, anvil next
+
+        Forged, // done on anvil, new base status, next is heated/cooled
+        
+        Heated, // heat on furnace after forging
+        Cooled, // cooled after heating
+
+        Sharpened, // status after whetstone
+        Completed // status after workbench
 
     };
 
